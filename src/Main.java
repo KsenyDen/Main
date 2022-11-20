6deffedf7e0141a5b248a46c4daebb14fbb0c548
Kseny <ksenyden@gmail.com>
2022-11-20 15:29:11 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,59 @@
+public class Main {
+    public static void main(String[] args) {
+        //Задание 1
+        System.out.println("Задание 1");
+        double dog = 8;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        System.out.println("Задание 2");
+        //Задание 2
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        System.out.println("Задание 3");
+        //Задание 3
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        System.out.println("Задание 4");
+        //Задача 4
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend/7;
+        System.out.println(friend);
+        System.out.println("Задание 5");
+        //Задача 5
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog*10;
+        System.out.println(frog);
+        frog = frog/3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+        //Задание 6
+        System.out.println("Задание 6");
+        var BoxerWeight1 = 78.2;
+        var BoxerWeight2 = 82.7;
+        var CommonWeight = BoxerWeight1 + BoxerWeight2;
+        System.out.println("Общий вес двух бойцов "+CommonWeight+" кг");
+        var WeightDifference = BoxerWeight2 - BoxerWeight1;
+        System.out.println("Разница в весе бойцов "+WeightDifference+" кг");
+        //Задание 7
+        System.out.println("Задание 7");
+        var WeightDifference2 = BoxerWeight2 % BoxerWeight1;
+        System.out.println("Разница в весе бойцов "+WeightDifference2+" кг");
+
+    }
+}
